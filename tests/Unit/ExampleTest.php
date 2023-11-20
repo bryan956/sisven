fea76c991aebc489374dfec50857a528273c12ff
bryan956 <131468909+bryan956@users.noreply.github.com>
2023-11-20 04:08:10 +0100

    login

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+test('that true is true', function () {
+    expect(true)->toBeTrue();
+});
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
